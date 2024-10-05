4fe14491820ac32252975c16302ab43792681435
Nikodem Jura <nikodem.jura@sabre.com>
2024-10-05 15:03:07 +0200

    Initial multi-module structure

---
+++ order-sync-service/src/main/java/com/company/order/sync/OrderSyncApplication.java
@@ -0,0 +1,13 @@
+package com.company.order.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSyncApplication.class, args);
+	}
+
+}
---
+++ order-sync-service/src/test/java/com/company/order/sync/OrderSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.order.sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
