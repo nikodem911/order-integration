e6781c12b1e4b6c84498cdebf4bc52dbfbfb1e4d
Nikodem Jura <nikodem.jura@sabre.com>
2024-10-07 09:26:47 +0200

    Add test for GRPC call to another JVM started in the same unit test.

--- order-sync-service/src/test/java/com/company/order/sync/OrderServiceInProcessTest.java
+++ order-sync-service/src/test/java/com/company/order/sync/OrderServiceInProcessTest.java
@@ -1,5 +1,10 @@
 package com.company.order.sync;
 
+import com.company.order.api.OrderSearchRequest;
+import com.company.order.api.OrderSearchResponse;
+import com.company.order.api.OrderServiceGrpc;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -9,8 +14,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Slf4j
 public class OrderServiceInProcessTest {
     private static Process process;
     private static final String REST_URL = "http://localhost:8080/orders";
@@ -33,7 +40,27 @@ public class OrderServiceInProcessTest {
         JsonOrder[] orders = restTemplate.getForObject(REST_URL, JsonOrder[].class);
 
         assertNotNull(orders, "Orders should not be null");
-        System.out.println(List.of(orders));
+        assertEquals(1, orders.length);
+        log.info("REST orders = {}", List.of(orders));
+    }
+
+    @Test
+    void retrieveOrdersGrpc() {
+        var channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+                .usePlaintext()
+                .build();
+        OrderServiceGrpc.OrderServiceBlockingStub orderService = OrderServiceGrpc.newBlockingStub(channel);
+
+        // when
+        OrderSearchResponse response = orderService.searchOrders(OrderSearchRequest.newBuilder().build());
+
+        // then
+        assertNotNull(response);
+        assertEquals(1, response.getOrdersList().size());
+        log.info("GRPC orders = {}", response.getOrdersList());
+
+        // Close the channel
+        channel.shutdown();
     }
 
     @AfterAll
